deec6b3cc6b02a23992800df88e79a9c2e787385
csanalosib <90751948+B4l0w@users.noreply.github.com>
2024-11-17 02:39:19 +0100

    Sokkal szebb, ha bevagy jelentkezve megjelenik a letöltés gomb a be-kijelentkezés gombok eltűnnek, kijelentkezel eltűnik és a gombok visszajönnek

--- src/Nav.js
+++ src/Nav.js
@@ -1,88 +1,15 @@
 import React, { useState } from 'react';
 import Kep from './img/profilkep.jpg';
-import './Nav.css';
+import './Nav.css'
 
 export default function Nav() {
-  let eldont = false;
-  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
-  const [isAboutVisible, setIsAboutVisible] = useState(false);
-  const [isLoginVisible, setIsLoginVisible] = useState(false);
-  const [isRegisterVisible, setIsRegisterVisible] = useState(false);
-  const [isSettingsVisible, setIsSettingsVisible] = useState(false);
 
-  // Bejelentkezéshez
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    alert(`Bejelentkezés: ${username}, Jelszó: ${password}`);
-  };
-
-  // Regisztrációhoz
-  const [registerUsername, setRegisterUsername] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
-  const [email, setEmail] = useState('');
-
-  const handleRegisterSubmit = (e) => {
-    e.preventDefault();
-    alert(`Regisztráció: Felhasználónév: ${registerUsername}, Email: ${email}, Jelszó: ${registerPassword}`);
-  };
-
-  // Beállításokhoz
-  const [newName, setNewName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-
-  const toggleDescription = () => {
-    setIsDescriptionVisible(true);
-  };
-
-  const closeDescription = () => {
-    setIsDescriptionVisible(false);
-  };
-
-  const toggleAbout = () => {
-    setIsAboutVisible(true);
-  };
-
-  const closeAbout = () => {
-    setIsAboutVisible(false);
-  };
-
-  const openLogin = () => {
-    setIsLoginVisible(true);
-  };
-
-  const closeLogin = () => {
-    setIsLoginVisible(false);
-  };
-
-  const openRegister = () => {
-    setIsRegisterVisible(true);
-  };
-
-  const closeRegister = () => {
-    setIsRegisterVisible(false);
-  };
-
-  const openSettings = () => {
-    setIsSettingsVisible(true);
-  };
-
-  const closeSettings = () => {
-    setIsSettingsVisible(false);
-  };
-
-  if (!isDescriptionVisible && !isAboutVisible && !isLoginVisible && !isRegisterVisible && !isSettingsVisible) {
-    eldont = false;
-  } else {
-    eldont = true;
-  }
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <nav className="navbar navbar-expand-lg custom-navbar">
-      <div style={{ display: eldont ? 'none' : 'block' }} className="container-fluid">
+      <div className="container-fluid">
         <button
           className="navbar-toggler"
           type="button"
@@ -98,24 +25,24 @@ export default function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" onClick={toggleDescription}>
+              <a className="nav-link" href="#descriptionModal" data-bs-toggle="modal">
                 Leírás
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" onClick={toggleAbout}>
+              <a className="nav-link" href="#aboutModal" data-bs-toggle="modal">
                 Rólunk
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" aria-disabled="true">
+            {isLoggedIn ? <li className="nav-item">
+              <a className="nav-link">
                 Letöltés
               </a>
-            </li>
+            </li> : ""}
           </ul>
 
-          <form className="d-flex ms-auto align-items-center">
-            <div className="dropdown">
+          <form className="d-flex ms-auto align-items-center text-center">
+            {isLoggedIn ? <div className="dropstart">
               <button
                 type="button"
                 id="dropdownMenuButton"
@@ -136,219 +63,193 @@ export default function Nav() {
                 />
               </button>
               <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                {isAdmin ? <li>
+                  <a className="dropdown-item" href="#settingsModal" data-bs-toggle="modal">
+                    Admin
+                  </a>
+                </li> : ""}
                 <li>
-                  <a className="dropdown-item" onClick={openSettings}>
+                  <a className="dropdown-item" href="#settingsModal" data-bs-toggle="modal">
                     Beállítások
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="#logout">
+                  <a onClick={() => setIsLoggedIn(false)} className="dropdown-item">
                     Kijelentkezés
                   </a>
                 </li>
               </ul>
-            </div>
-
-            <button className="btn btn-outline-light ms-2" onClick={openLogin}>
+            </div> : <><a className="btn btn-outline-light ms-2" data-bs-toggle="modal" data-bs-target="#loginModal">
               Bejelentkezés
-            </button>
-            <button className="btn btn-outline-light ms-2" onClick={openRegister}>
+            </a>
+            <a className="btn btn-outline-light ms-2" data-bs-toggle="modal" data-bs-target="#registerModal">
               Regisztráció
-            </button>
+            </a></>}
+            
+
+
           </form>
         </div>
       </div>
 
-      {/* Leírás menüpont */}
+      {/* Leírás Modal */}
       <div
-        className="centered-div"
-        style={{ display: isDescriptionVisible ? 'block' : 'none' }}
+        className="modal fade"
+        id="descriptionModal"
+        tabIndex="-1"
+        aria-labelledby="descriptionModalLabel"
+        aria-hidden="true"
       >
-        <button className="close-btn" onClick={closeDescription}>
-          X
-        </button>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
-      </div>
-
-      {/* Rólunk menüpont */}
-      <div
-        className="container centered-div"
-        style={{ display: isAboutVisible ? 'block' : 'none' }}
-      >
-        <button className="close-btn" onClick={closeAbout}>
-          X
-        </button>
-        <div className="row row text-center align-items-center">
-          <div className="col-4">
-            <h1>Nagy Levente Ferenc</h1>
-            <p>Aenean vel orci et purus faucibus consectetur...</p>
-          </div>
-          <div className="col-4">
-            <h1>Veller Árpád</h1>
-            <p>Aenean vel orci et purus faucibus consectetur...</p>
-          </div>
-          <div className="col-4">
-            <h1>Csanálosi Bálint</h1>
-            <p>Aenean vel orci et purus faucibus consectetur...</p>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="descriptionModalLabel">Leírás</h5>
+            </div>
+            <div className="modal-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia est eget ante
+              euismod, id sollicitudin leo malesuada. Integer convallis malesuada leo, ac auctor
+              lacus interdum et. Cras vestibulum, sapien at consequat tristique, risus metus
+              tincidunt orci, eu posuere ligula ligula non turpis. Etiam vitae efficitur libero.
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Bejelentkezés */}
+      {/* Rólunk Modal */}
       <div
-        className="centered-div"
-        style={{ display: isLoginVisible ? 'block' : 'none' }}
+        className="modal fade"
+        id="aboutModal"
+        tabIndex="-1"
+        aria-labelledby="aboutModalLabel"
+        aria-hidden="true"
       >
-        <button className="close-btn" onClick={closeLogin}>
-          X
-        </button>
-        <h2 className="text-center mb-4">Bejelentkezés</h2>
-        <form onSubmit={handleLoginSubmit}>
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Felhasználónév
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="username"
-              placeholder="Felhasználónév"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+        <div className="modal-dialog modal-dialog-centered modal-xl">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="aboutModalLabel">Rólunk</h5>
+            </div>
+            <div className="modal-body">
+              <div className="row text-center align-items-center">
+                <div className="col-md-4">
+                  <h1>Nagy Levente Ferenc</h1>
+                  <p>Aenean vel orci et purus faucibus consectetur. Nulla facilisi. Nulla
+                    consectetur magna eu dui ultricies, sed tincidunt tortor consectetur.</p>
+                </div>
+                <div className="col-md-4">
+                  <h1>Veller Árpád</h1>
+                  <p>Aenean vel orci et purus faucibus consectetur. Nulla facilisi. Nulla
+                    consectetur magna eu dui ultricies, sed tincidunt tortor consectetur.</p>
+                </div>
+                <div className="col-md-4">
+                  <h1>Csanálosi Bálint</h1>
+                  <p>Aenean vel orci et purus faucibus consectetur. Nulla facilisi. Nulla
+                    consectetur magna eu dui ultricies, sed tincidunt tortor consectetur.</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Jelszó
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Jelszó"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Bejelentkezés
-          </button>
-        </form>
+        </div>
       </div>
 
-      {/* Regisztráció */}
+      {/* Bejelentkezés Modal */}
       <div
-        className="centered-div"
-        style={{ display: isRegisterVisible ? 'block' : 'none' }}
+        className="modal fade"
+        id="loginModal"
+        tabIndex="-1"
+        aria-labelledby="loginModalLabel"
+        aria-hidden="true"
       >
-        <button className="close-btn" onClick={closeRegister}>
-          X
-        </button>
-        <h2 className="text-center mb-4">Regisztráció</h2>
-        <form onSubmit={handleRegisterSubmit}>
-          <div className="mb-3">
-            <label htmlFor="registerUsername" className="form-label">
-              Felhasználónév
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="registerUsername"
-              placeholder="Felhasználónév"
-              value={registerUsername}
-              onChange={(e) => setRegisterUsername(e.target.value)}
-            />
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="loginModalLabel">Bejelentkezés</h5>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">Felhasználónév</label>
+                  <input type="text" className="form-control" id="username" placeholder="Felhasználónév" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Jelszó</label>
+                  <input type="password" className="form-control" id="password" placeholder="Jelszó" />
+                </div>
+                <a onClick={() => setIsLoggedIn(true)} data-bs-dismiss="modal" className="btn btn-primary">Bejelentkezés</a>
+              </form>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="registerPassword" className="form-label">
-              Jelszó
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="registerPassword"
-              placeholder="Jelszó"
-              value={registerPassword}
-              onChange={(e) => setRegisterPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Regisztráció
-          </button>
-        </form>
+        </div>
       </div>
 
-      {/* Beállítások */}
+      {/* Regisztráció Modal */}
       <div
-        className="centered-div"
-        style={{ display: isSettingsVisible ? 'block' : 'none' }}
+        className="modal fade"
+        id="registerModal"
+        tabIndex="-1"
+        aria-labelledby="registerModalLabel"
+        aria-hidden="true"
       >
-        <button className="close-btn" onClick={closeSettings}>
-          X
-        </button>
-        <h2 className="text-center mb-4">Beállítások</h2>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="newName" className="form-label">
-              Új név
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="newName"
-              placeholder="Új név"
-              value={newName}
-              onChange={(e) => setNewName(e.target.value)}
-            />
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="registerModalLabel">Regisztráció</h5>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="registerUsername" className="form-label">Felhasználónév</label>
+                  <input type="text" className="form-control" id="registerUsername" placeholder="Felhasználónév" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input type="email" className="form-control" id="email" placeholder="Email" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="registerPassword" className="form-label">Jelszó</label>
+                  <input type="password" className="form-control" id="registerPassword" placeholder="Jelszó" />
+                </div>
+                <a className="btn btn-primary" data-bs-dismiss="modal">Regisztráció</a>
+              </form>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="newEmail" className="form-label">
-              Új email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="newEmail"
-              placeholder="Új email"
-              value={newEmail}
-              onChange={(e) => setNewEmail(e.target.value)}
-            />
+        </div>
+      </div>
+
+      {/* Beállítások Modal */}
+      <div
+        className="modal fade"
+        id="settingsModal"
+        tabIndex="-1"
+        aria-labelledby="settingsModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="settingsModalLabel">Beállítások</h5>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="newName" className="form-label">Új név</label>
+                  <input type="text" className="form-control" id="newName" placeholder="Új név" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="newEmail" className="form-label">Új email</label>
+                  <input type="email" className="form-control" id="newEmail" placeholder="Új email" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="newPassword" className="form-label">Új jelszó</label>
+                  <input type="password" className="form-control" id="newPassword" placeholder="Új jelszó" />
+                </div>
+                <div className="d-flex justify-content-end">
+                  <a className="btn btn-primary" data-bs-dismiss="modal">Módosítás</a>
+                  <a className="btn btn-danger ms-2" data-bs-dismiss="modal">Töröl</a>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="newPassword" className="form-label">
-              Új jelszó
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="newPassword"
-              placeholder="Új jelszó"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-          </div>
-          <div className="d-flex justify-content-end">
-          <button type="button" className="btn btn-primary">
-              Módosítás
-            </button>
-            <button type="button" className="btn btn-danger">
-              Töröl
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </nav>
   );

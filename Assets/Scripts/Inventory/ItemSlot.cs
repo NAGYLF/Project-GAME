8881938a7ada70cf8536882cd7786117c54c8b9a
NAGYLF <112727938+LEVISTKL@users.noreply.github.com>
2024-12-29 15:21:02 +0100

    V 0.1.5/j Minimap implementation fix

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using ItemHandler;
-using PlayerInventoryClass;
 using TMPro;
 
 namespace Assets.Scripts

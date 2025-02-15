b98a9b250a35477df194dd89ba864d974c7f88a6
Kuruzso <127405494+Kuruzso@users.noreply.github.com>
2025-02-15 20:59:29 +0100

    Delete Models directory

--- Models/Dto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EphemeralCourage_API.Models
-{
-    public class Dto
-    {
-        public record CreatePlayer(string Name, string Email, string Passworld,bool IsAdmin);
-        public record UpdatePlayer(string Name, string Email, string Passworld,bool IsAdmin);
-    }
-}

e69822a4a8825bb4ac6779eda5521c27461f628f
Kuruzso <127405494+Kuruzso@users.noreply.github.com>
2025-02-24 11:09:58 +0100

    Admin GetById is created

---
+++ AdminController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EphemeralApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EphemeralApi.Models.Dtos;
+
+namespace EphemeralApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly EphemeralCourageContext _context;
+
+        public AdminController(EphemeralCourageContext context)
+        {
+            _context = context;
+        }
+
+        // Összes admin lekérése
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmins()
+        {
+            return Ok(await _context.Admins.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Admin>> GetAdminById(int id)
+        {
+            var admin = await _context.Admins
+                .FirstOrDefaultAsync(a => a.Id == id); // Csak az Admin adatokat kérjük le
+
+            if (admin == null)
+            {
+                return NotFound(new { message = "Admin not found" });
+            }
+
+            return Ok(admin);
+        }
+
+        // Admin DevConsole beállításának frissítése
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDevConsole(int id, [FromBody] DevConsoleUpdateDto updateDto)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+                return NotFound(new { message = "Admin not found" });
+
+            admin.DevConsole = updateDto.DevConsole;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}

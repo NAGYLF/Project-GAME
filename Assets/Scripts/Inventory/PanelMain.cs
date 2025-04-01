fb6dd4e200f32c5e0020f3b82d133ad531f66aa1
NAGYLF <112727938+LEVISTKL@users.noreply.github.com>
2025-04-01 13:46:06 +0200

    V 0.1.8/b Healt Panle --> Equipmetn Panel open item miss fitt bug: fixed

--- Assets/Scripts/Inventory/PanelMain.cs
+++ Assets/Scripts/Inventory/PanelMain.cs
@@ -1,5 +1,9 @@
+using ItemHandler;
+using PlayerInventoryClass;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UI;
 using UnityEngine;
 
 public class PanelMain : MonoBehaviour
@@ -14,6 +18,10 @@ public class PanelMain : MonoBehaviour
     {
         HealthPanel.SetActive(false);
         EqipmentsPanel.SetActive(true);
+
+        //fix (ha heat panellel nyitjuk meg az inventory akkor az equipmenteket nem tudja fittelni.)
+        InGameUI.PlayerInventory.GetComponent<PlayerInventory>().levelManager.Items.Where(item => item.IsEquipment && item.SelfGameobject != null).ToList().ForEach(item => InventorySystem.ItemCompoundRefresh(item.SelfGameobject.GetComponent<ItemObject>().ItemCompound.GetComponent<ItemImgFitter>(),item));
+        //fix end
     }
     public void OpenHealtPanel()
     {
